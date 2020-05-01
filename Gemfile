69d0bf87dea902235d44bcb0fbaf2796ef25cc10
arslandogar <arslandogar112@yahoo.com>
2020-05-01 17:14:26 +0500

    edit post

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'carrierwave', '~> 2.0'
 gem 'pg_search'
 gem 'will_paginate', '~> 3.1', '>= 3.1.6'
 gem 'will_paginate-bootstrap4'
+gem 'cancancan'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
 # Use postgresql as the database for Active Record
@@ -47,6 +48,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'prettier'
 end
 
 group :test do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,15 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  load_and_authorize_resource
   
-
   def index
-    puts "here are paras"
-    puts params
-    puts "done"
     if params[:title] == nil
       @posts = Post.all.paginate(page: params[:page], per_page: 5)
     else
       @posts = Post.search_by_title(params[:title]).paginate(page: params[:page], per_page: 5)
     end
   end
+
   def new
     @post = current_user.posts.new
   end
@@ -19,10 +17,24 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.new(post_params)
     if @post.save
-      flash[:notice] = "Post was successfully created"
+      redirect_to @post
     else
       flash[:notice] = "NOOOOOOOOOOO"
       render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
   end
 
@@ -30,6 +42,14 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments.all
   end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    render 'show'
+  end
+
+
   private
   def post_params
     params.require(:post).permit(:title, :description, :image)
